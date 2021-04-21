67508fbb777bfa7db827a2b4b194316436ea1e1b
aaabdelgany <42511430+aaabdelgany@users.noreply.github.com>
2021-04-21 13:24:37 -0400

    reorganize

---
+++ part4/bloglist/app.js
@@ -0,0 +1,28 @@
+const config=require('./utils/config');
+const express=require('express');
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose');
+const blogRouter=require('./controllers/blogs');
+const middleware = require('./utils/middleware');
+const logger=require('./utils/logger');
+
+
+const mongoUrl = config.MONGODB_URI;
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+.then(()=>{
+    logger.info('Connected to MongoDB!');
+})
+.catch((error)=>{
+    logger.error(`error connecting to mongodb ${error.message}`);
+})
+
+app.use(cors());
+app.use(express.json());
+app.use(middleware.reqLogger);
+
+app.use('/api/blogs',blogRouter);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports=app;
