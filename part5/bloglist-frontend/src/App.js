b077f59bed6e644dfa5f47cd60f22e55487ee22b
aaabdelgany <42511430+aaabdelgany@users.noreply.github.com>
2021-05-05 17:11:54 -0400

    part 5a

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
-
+import loginService from './services/login';
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user,setUser] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,6 +14,36 @@ const App = () => {
     )  
   }, [])
 
+  const userLogin = async (e) => {
+    e.preventDefault();
+    console.log(`logging in with ${username} and ${password}`);
+
+    try{
+      const creds = {username,password};
+      const user = await loginService.login(creds);
+      setUser(user);
+      setUsername('');
+      setPassword('');
+      console.log('massive success!');
+      console.log(user);
+    }catch(error) {
+      console.log(error);
+    }
+  };
+  if(user===null){
+    return (
+      <div>
+        <form onSubmit={userLogin}>
+          <div>
+            username <input type="text" value={username} name = "username" onChange={({target})=>setUsername(target.value)}/>
+          </div>
+          <div>
+            password <input type="text" value={password} name = "password" onChange={({target})=>setPassword(target.value)} />
+          </div>
+          <button type="submit"> login</button>
+        </form>
+      </div>
+    )}
   return (
     <div>
       <h2>blogs</h2>
@@ -19,6 +52,6 @@ const App = () => {
       )}
     </div>
   )
-}
+};
 
 export default App
